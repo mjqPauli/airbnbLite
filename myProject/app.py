d62ee7924867f11762830642910f14254354a02b
Jianqi Ma <45346628+mjqPauli@users.noreply.github.com>
2019-10-01 19:32:27 -0400

    Added cookies and account page

--- myProject/app.py
+++ myProject/app.py
@@ -1,14 +1,35 @@
-from flask import Flask, Response, request, jsonify, render_template, make_response, flash 
+from flask import Flask, Response, request, jsonify, render_template, make_response, flash, session, redirect, url_for 
 from flask_pymongo import pymongo
 from bson.objectid import ObjectId
 from database import DatabaseConnection
+
+import datetime
 
 app = Flask(__name__)
 db = DatabaseConnection()
 app.secret_key = "airbnblite"
 
+
+def greeting():
+    name = request.cookies.get("signed_in")
+    hourTime = datetime.datetime.now().time().hour
+    greeting = ""
+    if not name:
+        return Response(status=404)
+    if hourTime < 12:
+        greeting = "Good Morning"
+    elif hourTime >= 12 and hourTime < 18:
+        greeting = "Good Afternoon"
+    else:
+        greeting = "Good Evening"
+    
+    response = greeting + ", " + name
+    return response
+
 @app.route('/login', methods = ["GET"])
 def loginForm():
+    if request.cookies.get("signed_in"):
+        return render_template("mode.html", GreetingMessage=greeting())
     return render_template('login.html')
 
 @app.route('/login', methods = ["POST"])
@@ -21,15 +42,16 @@ def login():
     if user == None:
         return Response("User doesn't exist!", status=200, content_type='text/html')
     if user["password"] == document["password"]:
-        return render_template("mode.html")
+        resp = make_response(redirect("/home"))
+        resp.set_cookie("signed_in", request.form["username"])
+        return resp
     else:
         return Response("Username and password mismatch!", status=200, content_type='text/html')
         
 @app.route('/home', methods = ["GET"])
 def mode():
-    return render_template("mode.html")
-
-
+    
+    return render_template("mode.html", GreetingMessage=greeting())
 
 @app.route('/signup', methods = ["GET"])
 def signupForm():
@@ -58,10 +80,34 @@ def addProperty():
     document = {
         "name": request.form["name"],
         "propertyType": request.form["type"],
-        "price": request.form["price"]
+        "price": request.form["price"],
+        "rentor": "",
+        "host": request.cookies.get("signed_in")
     }
     db.insert("properties", document)
     return Response("Property successfully added", status=200, content_type="text/html")
+
+@app.route('/rentProperty', methods = ['POST'])
+def rent():
+    document = {
+        "id" : request.form["property_id"]
+    }
+    property_rent = db.findOne("properties", {"_id" : ObjectId(document["id"])})
+    db.removeRented("properties", {"_id" : ObjectId(document["id"])})
+    property_rent["rentor"] = request.cookies.get("signed_in")
+    db.insert("properties", property_rent)
+    return Response("Property successfully rented", status=200, content_type="text/html")
+
+@app.route('/checkout', methods = ['POST'])
+def checkOut():
+    document = {
+        "id" : request.form["property_id"]
+    }
+    property_rent = db.findOne("properties", {"_id" : ObjectId(document["id"])})
+    db.removeRented("properties", {"_id" : ObjectId(document["id"])})
+    property_rent["rentor"] = ''
+    db.insert("properties", property_rent)
+    return Response('You have checked out!', status=200, content_type="text/html")
 
 @app.route('/removeProperty', methods = ['POST'])
 def deleteOne():
@@ -69,12 +115,25 @@ def deleteOne():
         "id" : request.form["property_id"]
     }
     db.removeRented("properties", {"_id" : ObjectId(document["id"])})
-    return Response("Property successfully rented", status=200, content_type="text/html")
+    return Response("Property successfully removed", status=200, content_type="text/html")
 
 @app.route('/properties', methods = ['GET'])
 def getProperties():
-    properties = db.findMany("properties", {})
+    properties = db.findMany("properties", {"rentor": ""})
     return render_template("properties.html", properties=properties)
+
+@app.route('/account', methods = ['GET'])
+def getAccount():
+    properties_host = db.findMany("properties", {"host": request.cookies.get("signed_in")})
+    properties_rent = db.findMany("properties", {"rentor": request.cookies.get("signed_in")})
+    name = request.cookies.get("signed_in")
+    return render_template("account.html", name=name, properties_host=properties_host, properties_rent=properties_rent)
+
+@app.route('/logout', methods = ['GET'])
+def logout():
+    resp = make_response(render_template("login.html"))
+    resp.set_cookie("signed_in", request.cookies.get("signed_in"), max_age=0)
+    return resp
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4000, debug=True)
